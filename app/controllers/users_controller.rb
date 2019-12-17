7837a09960249fa43ae26bb64a1171ed91618d69
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-17 15:28:43 -0500

    Send all UserWords to the frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,18 @@ class UsersController < ApplicationController
 
   end
 
-  def getWords
+  def get_words
+    UserLanguage.find_or_create_by(user_id: params[:user_id], language_id: params[:language_id])
+
+    user = User.find(params[:user_id])
+    user_language_words = user.find_user_language_words
     
+    user_language_words.each do |word|
+      UserWord.find_or_create_by( user_id: params[:user_id], word_id: word.id )
+    end
+    
+    user_words = UserWord.select { |word| word.user_id == user.id }
+
+    render json: user_words
   end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :languages, only: [:index]
   
   post "/login", to: "users#login"
-  post "/user_language", to: "users_languages#create"
+  post "/user_language", to: "users#get_words"
 end
