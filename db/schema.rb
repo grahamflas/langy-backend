63833c1d7b0ec6da4e2dd7b1bd30473765f2f546
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-17 07:04:58 -0500

    Add slug to languages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_161337) do
+ActiveRecord::Schema.define(version: 2019_12_16_225303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_161337) do
     t.string "language_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
   end
 
   create_table "user_languages", force: :cascade do |t|
