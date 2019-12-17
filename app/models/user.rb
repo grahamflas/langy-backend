ef1cc3f0df6153894cebeee67d46d2423b1a020e
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-17 11:35:47 -0500

    Define function to grab all words in all of user's languages

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
   has_many :user_words
   has_many :words, through: :user_words
 
+  # ##Find all of the words in the languages the user is studying
+  def find_user_language_words
+    words_array = []
+
+    self.languages.each do |lang|
+      lang.words.each do |word|
+        words_array << word
+      end
+    end
+
+    words_array
+  end
+
 end
+
+
+# user_language_words.each {|word| UserWord.find_or_create_by(user: user, word: word)}
