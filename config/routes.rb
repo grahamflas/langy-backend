95e7f6f44590094d5feb517c346340e74b7cdb2f
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-18 10:38:30 -0500

    Change route for POST to get user-words

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :languages
   
   post "/login", to: "users#login"
-  post "/user_language", to: "users#get_words"
+  post "/get_words", to: "users#get_words"
   post "/get_decks", to: "decks#get_decks"
 end
